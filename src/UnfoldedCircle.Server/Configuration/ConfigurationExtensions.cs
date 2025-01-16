b16485b55776bfa9afa55afe0be500bcc143baca
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2025-01-16 20:40:58 +0100

    Remove unused method

--- src/UnfoldedCircle.Server/Configuration/ConfigurationExtensions.cs
+++ src/UnfoldedCircle.Server/Configuration/ConfigurationExtensions.cs
@@ -4,16 +4,6 @@ namespace UnfoldedCircle.Server.Configuration;
 
 public static class ConfigurationExtensions
 {
-    public static T GetRequired<T>(this IConfiguration configuration, string key)
-        where T : IParsable<T>
-    {
-        var s = configuration[key];
-        if (T.TryParse(s, CultureInfo.InvariantCulture, out var value))
-            return value;
-        
-        throw new InvalidOperationException($"Could not parse value '{s}' for key '{key}'");
-    }
-    
     public static T GetOrDefault<T>(this IConfiguration configuration, string key, T defaultValue)
         where T : IParsable<T> =>
         T.TryParse(configuration[key], CultureInfo.InvariantCulture, out var value) ? value : defaultValue;
