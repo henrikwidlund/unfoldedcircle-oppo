a3e5f350ea23c6b327e87897a39f8a9f0d71874f
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2025-08-31 21:46:45 +0200

    Fix linting

--- src/UnfoldedCircle.OppoBluRay/WebSocket/OppoWebSocketHandler.Oppo.cs
+++ src/UnfoldedCircle.OppoBluRay/WebSocket/OppoWebSocketHandler.Oppo.cs
@@ -25,10 +25,10 @@ public partial class OppoWebSocketHandler
         var entity = identifierType switch
         {
             IdentifierType.DeviceId => localIdentifier is { Span.IsEmpty: false }
-                ? configuration.Entities.Find(x => x.DeviceId?.AsMemory().Span.Equals(localIdentifier.Value.Span, StringComparison.OrdinalIgnoreCase) == true)
+                ? configuration.Entities.Find(x => x.DeviceId?.AsMemory().Span.Equals(localIdentifier.Value.Span, StringComparison.OrdinalIgnoreCase) is true)
                 : configuration.Entities[0],
             IdentifierType.EntityId => localIdentifier is { Span.IsEmpty: false }
-                ? configuration.Entities.Find(x => x.EntityId.AsMemory().Span.Equals(localIdentifier.Value.Span, StringComparison.OrdinalIgnoreCase) == true)
+                ? configuration.Entities.Find(x => x.EntityId.AsMemory().Span.Equals(localIdentifier.Value.Span, StringComparison.OrdinalIgnoreCase))
                 : null,
             _ => throw new ArgumentOutOfRangeException(nameof(identifierType), identifierType, null)
         };
