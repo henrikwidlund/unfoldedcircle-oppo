cb0fc04b450d5190e934d53bf547951805a720c3
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2026-01-16 01:27:53 +0100

    Adjust WoL

--- src/Oppo/WakeOnLan.cs
+++ src/Oppo/WakeOnLan.cs
@@ -5,14 +5,14 @@ namespace Oppo;
 
 internal static class WakeOnLan
 {
-    private static readonly IPAddress BroadcastAddress = IPAddress.Parse("255.255.255.255");
+    private static readonly IPEndPoint BroadcastIpEndPoint = new(IPAddress.Parse("255.255.255.255"), 9);
 
     public static async ValueTask SendWakeOnLanAsync(string macAddress)
     {
         byte[] magicPacket = CreateMagicPacket(macAddress);
-        using var socket = new Socket(BroadcastAddress.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+        using var socket = new Socket(BroadcastIpEndPoint.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
         socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
-        await socket.SendToAsync(magicPacket, new IPEndPoint(BroadcastAddress, 9));
+        await socket.SendToAsync(magicPacket, BroadcastIpEndPoint);
     }
 
     private static byte[] CreateMagicPacket(string macAddress) =>
