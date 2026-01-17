df60008c7fb078661050ad56797a865f1b293d64
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2026-01-17 17:13:58 +0100

    Send WoL on port 7 and 9

--- src/Oppo/WakeOnLan.cs
+++ src/Oppo/WakeOnLan.cs
@@ -5,14 +5,16 @@ namespace Oppo;
 
 internal static class WakeOnLan
 {
-    private static readonly IPEndPoint BroadcastIpEndPoint = new(IPAddress.Parse("255.255.255.255"), 9);
+    private static readonly IPEndPoint BroadcastIpEndPointPort7 = new(IPAddress.Parse("255.255.255.255"), 7);
+    private static readonly IPEndPoint BroadcastIpEndPointPort9 = new(IPAddress.Parse("255.255.255.255"), 9);
 
     public static async ValueTask SendWakeOnLanAsync(string macAddress)
     {
         byte[] magicPacket = CreateMagicPacket(macAddress);
-        using var socket = new Socket(BroadcastIpEndPoint.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+        using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
-        await socket.SendToAsync(magicPacket, BroadcastIpEndPoint);
+        await socket.SendToAsync(magicPacket, BroadcastIpEndPointPort7);
+        await socket.SendToAsync(magicPacket, BroadcastIpEndPointPort9);
     }
 
     private static byte[] CreateMagicPacket(string macAddress) =>
